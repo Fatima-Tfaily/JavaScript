d07abed086d749d6e64cc8904d7c615f1d6ded19
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-07-25 10:46:06 +0300

    create script

---
+++ task_1/js1.js
@@ -0,0 +1,9 @@
+
+    var u = document.getElementById ("user");
+    var p = document.getElementById("pass");
+    var n = document.getElementById("name");
+    function myfunction(u,p,n){
+    if(u!=""&&p!=""&&n!=""){
+        alert("hello");
+    }
+}
