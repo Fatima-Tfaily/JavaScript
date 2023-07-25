276ef7808785d104ce5c94b874a07e4cfbbbc1e0
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-07-25 15:53:11 +0300

    Done task3

--- task_3/task3.js
+++ task_3/task3.js
@@ -1,17 +1,16 @@
 const num1 = 2;
 var num2 = 3;
-if(num1>=num2){
-    console.log("hello");
-}
-else{
-    console.log("Bye");
-}
-num2 = 5;
-console.log(num2);
 var canPass = true;
 if(canPass==true){
-    console.log("")
-}
+    if(num1>=num2){
+       console.log("hello");
+     }
+    else{
+    console.log("Bye");
+     }
+ }
 else{
     console.log("Try Again");
 }
+num2 = 5;
+console.log(num2);
