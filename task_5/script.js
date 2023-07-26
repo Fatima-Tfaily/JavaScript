3ae72ef2180e9d8c60360015b77910a8c6374d43
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-07-26 12:28:27 +0300

    Done task 5

---
+++ task_5/script.js
@@ -0,0 +1,9 @@
+let number = [1,2,4,6,8,11,45,98,102,66,73];
+for(let i=0 ; i<number.length ; i++){
+    if(number[i] % 2 == 0){
+        console.log(number[i]+" is even");
+    }
+    else{
+        console.log(number[i]+" is odd");
+    }
+}
