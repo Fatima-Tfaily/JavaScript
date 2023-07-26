d481485f4dff61cb2e6ef2fdc09e22bd92cf51f9
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-07-26 10:28:23 +0300

    Done task4

---
+++ task_4/task4.js
@@ -0,0 +1,8 @@
+var array1 = [4 , 7 , 10 , 20 , 15];
+var array2 = [];
+var array3 = new Array();
+array2 = ["element1" , "element2" , "element3"];
+array3 = ["element1" , 4 , "element2" , 20];
+console.log("Array1:"+array1);
+console.log("Array2:"+array2);
+console.log("Array3:"+array3);
