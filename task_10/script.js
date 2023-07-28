1a0a1795bbc1be6c5557d8e1cfc8dafdebdead27
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-07-28 10:59:16 +0300

    Done With task10

--- task_10/script.js
+++ task_10/script.js
@@ -3,10 +3,6 @@ const modal = document.getElementById("modal");
 const modalTitle = document.getElementById("modalTitle");
 const closeBtn = document.getElementById("closeBtn");
 const modalContent = document.querySelector(".modal-content");
-function closeModal() {
-  modal.style.display = "none";
-}
-closeBtn.addEventListener("click", closeModal());
 openBtn.addEventListener("click", function(){
     modal.style.display = "block";
     modal.html = modalTitle;
