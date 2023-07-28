d75141883bf882a8fca1a5051ebe0c4c61857571
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-07-28 13:32:57 +0300

    Done With Task-10

--- task_10/script.js
+++ task_10/script.js
@@ -1,16 +1,23 @@
-const openBtn = document.getElementById("openBtn");
-const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("myModal");
 const modalTitle = document.getElementById("modalTitle");
-const closeBtn = document.getElementById("closeBtn");
+const closeBtn = document.querySelector(".close");
 const modalContent = document.querySelector(".modal-content");
 openBtn.addEventListener("click", function(){
+   const userInput = prompt("Enter the modal title:");
+    if (userInput) {
+    modalTitle.textContent="";
+    modalTitle.textContent = userInput;
     modal.style.display = "block";
-    modal.html = modalTitle;
-})
-closeBtn.addEventListener("click", function(){
-    modal.style.display = 'none';
+    modal.html = modalContent;
 }
-);
-modalContent.addEventListener("click",function(){
-    modal.style.display = 'none';
+}
+)
+closeBtn.addEventListener("click",function(){
+    modal.style.display = "none";
 })
+window.addEventListener("click",function(event){
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
